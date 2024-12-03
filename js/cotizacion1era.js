5ddf78f5403a662c74d91d1df5f8add14c32beee
Axel Bouvier <axelbouvierm@gmail.com>
2024-12-03 17:13:21 -0300

    test

---
+++ js/cotizacion1era.js
@@ -0,0 +1,152 @@
+let button = document.querySelector('#buttonSend')
+let cui = document.querySelector('#inputCUI')
+
+cui.addEventListener("change", (event) => {
+
+    if (+cui.value > 0) {
+        document.querySelector('#integraciones').removeAttribute('hidden');
+    } else {
+        document.querySelector('#integraciones').setAttribute('hidden', '');
+    }
+})
+
+button.addEventListener('click', crearCoti)
+/* Al seelccionar uno de los tipos de bots que cambie la view para completar los datos del respectivo bot.
+Seleccionar tipo de partner. */
+function crearCoti() {
+    let botType = window.localStorage.getItem('botType')
+    let cub = +document.querySelector('#inputCUB').value
+    cui = +document.querySelector('#inputCUI').value
+    let integraciones = document.querySelector('#inputIntegraciones')
+    let sesionesHey = document.querySelector('#inputSesionesHey').value.split('-')
+    let soporte = document.querySelector('#inputSoporte').value.split('-')
+
+    // Verificar si VPN está seleccionado
+    let vpnSi = document.querySelector('#inputVPNYes').checked
+    let vpnNo = document.querySelector('#inputVPNNo').checked
+
+    // Verifica si ImageBot, Voice2Text y Catálogo están seleccionados
+    let imagebot = document.querySelector('#inputImageBot').checked
+    let voice2text = document.querySelector('#inputVoice2Text').checked
+    let catalogo = document.querySelector('#inputCatalogo').checked
+
+    let error =  document.querySelector('#error')
+    error.innerHTML = ''
+
+    let desc = ''
+    let totalSetUp = 990
+    let horasSetUp = 30
+
+    if (cub > 0) {
+        desc += `- ${cub} casos de uso sin integraciones<br>`
+        totalSetUp += (cub * 500)
+        horasSetUp += (cub * 10)
+    }
+    if (cui > 0) {
+        if (+integraciones.value > 0) {
+            integraciones.style.border = "";
+            desc += `- ${cui} casos de uso con ${integraciones.value} integraciones<br>`
+            totalSetUp += (cui * 900)
+            horasSetUp += (cui * 15) + (+integraciones.value * 5)
+        } else {
+            integraciones.style.border = "1px solid red";
+            error.innerHTML = 'Es necesario indicar la cantidad de integraciones'
+            return;
+        }
+    }
+
+    let coti = [
+        {
+            item: 'Bot de 1era generación',
+            descr: desc,
+            precio: totalSetUp,
+            tipoPago: 'Pago inicial por unica vez',
+            horas: horasSetUp
+        }
+    ]
+
+    let itemSesiones = ''
+    let totalSesiones = 0
+    if (+sesionesHey[0] > 0) {
+        itemSesiones = `${+sesionesHey[0]} Sesiones Hey`
+        totalSesiones = +sesionesHey[1]
+        coti.push({
+            item: itemSesiones,
+            descr: '-',
+            precio: totalSesiones,
+            tipoPago: 'Pago Mensual',
+            horas: 0
+        })
+    }
+
+    // Solo agregar VPN si está marcado
+    if (vpnSi) {
+        coti.push({
+            item: 'VPN',
+            descr: 'Conexión VPN para los servicios',
+            precio: 360,
+            tipoPago: 'Pago inicial por unica vez',
+            horas: 8
+        })
+        coti.push({
+            item: 'VPN',
+            descr: 'Conexión VPN para los servicios',
+            precio: 190,
+            tipoPago: 'Pago Mensual',
+            horas: 8
+        })
+    }
+
+    // Solo agregar Imagebot si está marcado
+    if (imagebot) {
+        coti.push({
+            item: 'Imagebot',
+            descr: '- Documentos con formato fijo / variable',
+            precio: 2800,
+            tipoPago: 'Pago inicial por unica vez',
+            horas: 80
+        })
+    }
+
+    // Solo agregar Voice2Text si está marcado
+    if (voice2text) {
+        coti.push({
+            item: 'Voice2Text',
+            descr: '-',
+            precio: 800,
+            tipoPago: 'Pago inicial por unica vez',
+            horas: 20
+        })
+    }
+
+    if (catalogo) {
+        coti.push({
+            item: 'Catálogo',
+            descr: '- Catálogo de WhatsApp',
+            precio: 900,
+            tipoPago: 'Pago inicial por unica vez',
+            horas: 20
+        })
+    }
+
+    if (soporte[0] > 0) {
+        coti.push({
+            item: `Soporte ${soporte[2]}`,
+            descr: `- Soporte extendido (${soporte[0]} horas de dedicación)`,
+            precio: +soporte[1],
+            tipoPago: 'Pago mensual',
+            horas: 0
+        })
+    } else {
+        coti.push({
+            item: `Soporte básico`,
+            descr: `- Soporte básico (${sesionesHey[0] == '2500' ? '4 horas' : '8 horas'} de dedicación)`,
+            precio: +soporte[1],
+            tipoPago: 'Pago mensual',
+            horas: 0
+        })
+    }
+
+    window.localStorage.setItem('coti', JSON.stringify(coti))
+    window.location.href = 'cotizacionRealizada.html'
+}
---
+++ js/cotizacionVoicebot.js
@@ -0,0 +1,113 @@
+let button = document.querySelector('#buttonSend')
+
+button.addEventListener('click', crearCoti)
+/* Al seelccionar uno de los tipos de bots que cambie la view para completar los datos del respectivo bot.
+Seleccionar tipo de partner. */
+function crearCoti() {
+    let botType = window.localStorage.getItem('botType')
+    let cub = +document.querySelector('#inputCUB').value
+    let cui = +document.querySelector('#inputCUI').value
+    let knowledgeSi = document.querySelector('#inputAssistantYes').checked
+    let knowledgeNo = document.querySelector('#inputAssistantNo').checked
+    let sesionesHey = document.querySelector('#inputSesionesHey').value.split('-')
+    let sesionesHeyIA = document.querySelector('#inputSesionesHeyIA').value.split('-')
+
+    // Verificar si VPN está seleccionado
+    let vpnSi = document.querySelector('#inputVPNYes').checked
+    let vpnNo = document.querySelector('#inputVPNNo').checked
+
+    // Verifica si ImageBot y Voice2Text están seleccionados
+    let imagebot = document.querySelector('#inputImageBot').checked
+    let voice2text = document.querySelector('#inputVoice2Text').checked
+    
+    let desc = '- 1 caso de uso con integraciones<br>'
+    let totalSetUp = 3400
+    let horasSetUp = 85
+
+    if (cub > 0) {
+        desc += `- ${cub} casos de uso sin integraciones<br>`
+        totalSetUp += (cub * 500)
+        horasSetUp += (cub * 10)
+    }
+    if (cui > 0) {
+        desc += `- ${cui} casos de uso con integraciones<br>`
+        totalSetUp += (cui * 900)
+        horasSetUp += (cui * 15)
+    }
+
+    if (knowledgeSi) {
+        desc += `- Asistente conversacional<br>`
+        totalSetUp += 1000
+        horasSetUp += 15
+    }
+
+    let coti = [
+        { 
+            item: botType = 'Voicebot', 
+            descr: desc, 
+            precio: totalSetUp, 
+            tipoPago: 'Pago inicial por unica vez', 
+            horas: horasSetUp 
+        }
+    ]
+
+    let itemSesiones = '' 
+    let totalSesiones = 0
+    if (+sesionesHey[0] > 0) {
+        itemSesiones = `${+sesionesHey[0]} Sesiones Hey`
+        totalSesiones = +sesionesHey[1]
+        coti.push({ 
+            item: itemSesiones,
+            descr: '-', 
+            precio: totalSesiones, 
+            tipoPago: 'Pago Mensual', 
+            horas: 0 
+        })
+    }
+
+    if (+sesionesHeyIA[0] > 0) {
+        itemSesiones = `${+sesionesHeyIA[0]} Sesiones Hey con IA`
+        totalSesiones = +sesionesHeyIA[1]
+        coti.push({ 
+            item: itemSesiones,
+            descr: '-', 
+            precio: totalSesiones, 
+            tipoPago: 'Pago Mensual', 
+            horas: 0 
+        })
+    }
+    // Solo agregar VPN si está marcado
+    if (vpnSi) {
+        coti.push({ 
+            item: 'VPN',
+            descr: 'Conexión VPN para los servicios', 
+            precio: 360, 
+            tipoPago: 'Pago Mensual', 
+            horas: 8
+        })
+    }
+
+    // Solo agregar Imagebot si está marcado
+    if (imagebot) {
+        coti.push({ 
+            item: 'Imagebot',
+            descr: '-', 
+            precio: 2000, 
+            tipoPago: 'Pago inicial por unica vez', 
+            horas: 70
+        })
+    }
+
+    // Solo agregar Voice2Text si está marcado
+    if (voice2text) {
+        coti.push({ 
+            item: 'Voice2Text',
+            descr: '-', 
+            precio: 800, 
+            tipoPago: 'Pago inicial por unica vez', 
+            horas: 20
+        })
+    }
+    window.localStorage.setItem('coti', JSON.stringify(coti))
+    window.location.href = 'cotizacionRealizada.html'
+}
